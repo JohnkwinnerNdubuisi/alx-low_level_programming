67b74f455b9f8f38ab54d92bc7b91fc35255f441
Johnk Okeke <okekejohnk@gmail.com>
2022-12-19 09:26:54 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,12 +5,11 @@
 
 
 /**
+ * main - reset_to_98 - update pointer value to 98
  *
- *  * reset_to_98 - update pointer value to 98
+ * @n: parameter to be provided
  *
- *   * @n: parameter to be provided
- *
- *    * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
 */
 
 
