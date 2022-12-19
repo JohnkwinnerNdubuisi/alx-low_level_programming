63b51750e0ce2632727f719fe6614d0d79596329
Johnk Okeke <okekejohnk@gmail.com>
2022-12-19 10:19:12 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include "main.h"
+
+
+
+/**
+ * _strlen - a function to print the length of a string
+ *
+ * @s: string
+ *
+ * Return : length
+ *
+*/
+
+
+
+int _strlen(char *s)
+
+{
+
+		int i = 0;
+	while (s[i] != '\0')
+
+					{
+
+								i++;
+
+									}
+
+
+
+				return (i);
+
+}
