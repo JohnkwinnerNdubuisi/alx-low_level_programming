53beaf294c4b2e97ae52bf243f1f4bd9e8303659
Johnk Okeke <okekejohnk@gmail.com>
2023-01-02 09:19:30 -0800

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte.
+ * fills the first n bytes of the memory area pointed
+ * to by s with the constant b.
+ * @s: pointer to memory area.
+ * @b: constant to fill memory with.
+ * @n: bytes of the memory area to be filled.
+
+ Return: 0
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+	unsigned int counter;
+
+	for (counter = 0; counter < n; counter++)
+
+	{
+		s[counter] = b;
+	}
+
+	return (0);
+
+}
